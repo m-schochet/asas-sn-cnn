e9c8641c9da418a62b0cd2107e2a8afba6013ff3
Meir Schochet <103089439+m-schochet@users.noreply.github.com>
2025-05-23 09:49:49 -0400

    Create run_sims.sh

---
+++ butterpy-simulations/run_sims.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#SBATCH --job-name=(INSERT YOUR JOB NAME HERE)
+#SBATCH --qos=(INSERT YOUR RESOURCE ALLOCATION NAME HERE)
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=(INSERT YOUR EMAIL FOR JOB NOTIFICATIONS HERE)
+#SBATCH --mem=200MB
+#SBATCH --time=08:00:00
+#SBATCH --output=output/%x-%A-%03a.out
+#SBATCH --error=output/%x-%A-%03a.err
+
+#SBATCH --array=0-999%100
+module load conda
+conda activate (NAME OF ENVIRONMENT HOSTING THE PACKAGES IN REQUIREMENTS.TXT)
+python run_sims.py $SLURM_ARRAY_TASK_ID
