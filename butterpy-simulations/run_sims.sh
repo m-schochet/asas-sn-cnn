5900304843f3171ef95a93f75808806f62f00978
Meir Schochet <103089439+m-schochet@users.noreply.github.com>
2025-05-23 09:56:07 -0400

    Update run_sims.sh

--- butterpy-simulations/run_sims.sh
+++ butterpy-simulations/run_sims.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=08:00:00
 #SBATCH --output=output/%x-%A-%03a.out
 #SBATCH --error=output/%x-%A-%03a.err
-#SBATCH --array=0-999%100
+#SBATCH --array=0-999%100  # Each job uses 200MB of memory, resulting in total resource request of 200 GB of RAM
 
 module load conda
 conda activate (NAME OF ENVIRONMENT HOSTING THE PACKAGES IN REQUIREMENTS.TXT)
