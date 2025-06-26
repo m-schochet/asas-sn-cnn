40610a8e986784f1ade33ad5a7dae1dcef4c1fee
nitraralts <55763623+nitraralts@users.noreply.github.com>
2025-06-26 12:03:43 -0400

    Update download_milliquas.py
    added the word "quiescent"...

--- 0-get-quiescent-bkg/download_milliquas.py
+++ 0-get-quiescent-bkg/download_milliquas.py
@@ -11,7 +11,7 @@ SELECT *
 FROM milliquas 
 """
 
-# The variable `save_path` is the location on your device for where to save the light curves. Insert your own respective save path here to store the light curves
+# The variable `save_path` is the location on your device for where to save the quiescent light curves. Insert your own respective save path here to store the light curves
 
 # Note: there are—as expected from the "Million Quasar Catalog"—about one million quasar light curves stored in this catalog at the ASAS-SN Servers. Since the quantity of files is quite large, ensure that you have at least 200GB of available storage in the location where you intend to download these files
 
