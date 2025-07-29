8479f06affa7e60fb41b788e8460f29e5be641f6
Meir Schochet <meirschochet13@gmail.com>
2025-07-29 14:55:15 -0400

    Update create_folders.sh

--- 1-inject-noise/create_folders.sh
+++ 1-inject-noise/create_folders.sh
@@ -4,10 +4,10 @@
 #SBATCH --qos=(INSERT YOUR RESOURCE ALLOCATION NAME HERE)
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=(INSERT YOUR EMAIL FOR JOB NOTIFICATIONS HERE)
+#SBATCH --output=output/%x-%A-%04a.out (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
+#SBATCH --error=output/%x-%A-%04a.err (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
 #SBATCH --mem=2GB
 #SBATCH --time=01:00:00
-#SBATCH --output=output/%x-%A-%03a.out
-#SBATCH --error=output/%x-%A-%03a.err
 #SBATCH --array=0-999%100
 
 module load conda
