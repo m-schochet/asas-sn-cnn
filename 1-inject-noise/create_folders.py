37be82b6a449a6b0275307124a171f1acbc26d43
Meir Schochet <meirschochet13@gmail.com>
2025-07-30 21:41:20 -0400

    Update create_folders.py

--- 1-inject-noise/create_folders.py
+++ 1-inject-noise/create_folders.py
@@ -3,10 +3,10 @@ import sys
 import numpy as np
 
 def run(jobid):
-    # Begin by making the folder and save location for our noisy injected curves. It will generate a number of folders equal to the
-    # Number of jobs ran
+    # Begin by making the folder and save location for our noisy injected curves. 
+    # This bit of code will generate a number of folders equal to the number of jobs ran
 
-    outpath = os.path.join("training_wavelets", f"{jobid:03d}")
+    outpath = os.path.join("(#path to save injected light curves#"), "training_wavelets", f"{jobid:03d}")
     os.makedirs(outpath, exist_ok=False)
 
 if __name__ == "__main__":
