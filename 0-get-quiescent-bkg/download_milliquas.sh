977933a09b63b6a151cc10ac606fa1de1bd1f289
Meir Schochet <meirschochet13@gmail.com>
2025-07-30 21:21:07 -0400

    Update download_milliquas.sh

--- 0-get-quiescent-bkg/download_milliquas.sh
+++ 0-get-quiescent-bkg/download_milliquas.sh
@@ -4,14 +4,13 @@
 #SBATCH --qos=(INSERT YOUR RESOURCE ALLOCATION NAME HERE)
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=(INSERT YOUR EMAIL FOR JOB NOTIFICATIONS HERE)
+#SBATCH --output=output/%x-%A-%04a.out (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
+#SBATCH --error=output/%x-%A-%04a.err (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
 #SBATCH --time=04:00:00
 #SBATCH --nodes=1                    # Run all processes on a single node
 #SBATCH --ntasks=1                   # Run a single task                
 #SBATCH --cpus-per-task=32           # Number of CPU cores per task
 #SBATCH --mem-per-cpu=4G             # 128 GB of RAM total
-#SBATCH --output=output/%x-%A-%04a.out (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
-#SBATCH --error=output/%x-%A-%04a.err (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
-
 
 module load conda
 conda activate (NAME OF ENVIRONMENT HOSTING THE PACKAGES IN REQUIREMENTS.TXT)
