a6ff48c91e4de5c7475598cc406ab3aaa03c10b1
Meir Schochet <meirschochet13@gmail.com>
2025-07-30 21:20:11 -0400

    Update compile_all_sims.sh

--- butterpy-simulations/compile_all_sims.sh
+++ butterpy-simulations/compile_all_sims.sh
@@ -4,10 +4,11 @@
 #SBATCH --qos=(INSERT YOUR RESOURCE ALLOCATION NAME HERE)
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=(INSERT YOUR EMAIL FOR JOB NOTIFICATIONS HERE)
+#SBATCH --output=output/%x-%A-%04a.out (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
+#SBATCH --error=output/%x-%A-%04a.err (ENSURE YOU HAVE A FOLDER NAMED `output` IN THE LOCATION YOU RUN THIS JOB)
 #SBATCH --mem=2GB            # Total memory usage for this job is 2GB of RAM
 #SBATCH --time=08:00:00
-#SBATCH --output=output/%x-%A-%03a.out
-#SBATCH --error=output/%x-%A-%03a.err
+
 
 module load conda
 conda activate (NAME OF ENVIRONMENT HOSTING THE PACKAGES IN REQUIREMENTS.TXT)
