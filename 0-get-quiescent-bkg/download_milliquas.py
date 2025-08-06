77f1591255a065d8d35b09e98bf6747828f62970
m-schochet <schoche4@msu.edu>
2025-08-06 10:39:47 -0500

    add first docstrings to 0 and 0.5 .py files

--- 0-get-quiescent-bkg/download_milliquas.py
+++ 0-get-quiescent-bkg/download_milliquas.py
@@ -11,9 +11,13 @@ SELECT *
 FROM milliquas 
 """
 
-# The variable `save_path` is the location on your device for where to save the quiescent light curves. Insert your own respective save path here to store the light curves
+# The variable `save_path` is the location on your device for where to save the quiescent light curves. 
+# Insert your own respective save path here to store the light curves
 
-# Note: there are—as expected from the "Million Quasar Catalog"—about one million quasar light curves stored in this catalog at the ASAS-SN Servers. Since the quantity of files is quite large, ensure that you have at least 200GB of available storage in the location where you intend to download these files
+# Note: there are—as expected from the "Million Quasar Catalog"—about one million quasar light curves stored 
+# in this catalog at the ASAS-SN Servers. 
+# Since the quantity of files is quite large, ensure that you have at least 200GB of available storage 
+# in the location where you intend to download these files
 
 save_path = '(# insert your path here #)'
 
